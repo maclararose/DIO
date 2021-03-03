02957f1d54c064cd7a74d901949162fc4cd13691
maclararose <mcbarroso.contato@gmail.com>
2021-03-02 23:42:14 -0300

    First data struct: Stack

---
+++ dotnet - I/Pilha/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pilha{
+    class Program{
+        static void Main(){
+            var stack = new Pilha();
+            stack.Empilha(1);
+            stack.Empilha(10);
+            stack.Empilha(6);
+            stack.Empilha(8);
+            stack.Empilha(14);
+            stack.Empilha(11);
+
+            Console.WriteLine(stack.Desempilha());
+            Console.WriteLine(stack.Desempilha());
+            Console.WriteLine(stack.Desempilha());
+        }
+    }
+}
