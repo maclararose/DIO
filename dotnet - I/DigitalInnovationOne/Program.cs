1e286d7fda257fa319a299983eb29e1351ea1cf1
maclararose <mcbarroso.contato@gmail.com>
2021-03-02 22:44:15 -0300

    First test in C#

---
+++ dotnet - I/DigitalInnovationOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            for(int i = 0; i < number; i++){
+                i++;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
